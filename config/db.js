97e3114b3a3e1ff1139535e8477683ff4ae31d61
Osiris8 <miganosiris8@gmaaail.com>
2023-08-17 09:01:33 +0200

    add database config MongoDB

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 // Define the database URL to connect to.
-const mongoDB = "mongodb+srv://osiris:osiris@cluster0.z3a2i.mongodb.net/";
+const mongoDB = "mongodb+srv://osiris:osiris@cluster0.z3a2i.mongodb.net/source";
 
 // Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
