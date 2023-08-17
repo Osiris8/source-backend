2bff8fd61b0a74f173e36aaacaf9aeec988a9480
Osiris8 <miganosiris8@gmaaail.com>
2023-08-17 09:16:59 +0200

    add post, get, put and delete route

---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
+
+///Auth authentification
+router.post("/register", authController.signUp);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+
+module.exports = router;
