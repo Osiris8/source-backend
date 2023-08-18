f87b77ebb5c492b184f01e326c6bb6ad79484359
Osiris8 <miganosiris8@gmaaail.com>
2023-08-18 17:53:49 +0200

    delete userSchema.statics.login

--- models/user.model.js
+++ models/user.model.js
@@ -56,17 +56,6 @@ userSchema.pre("save", async function (next) {
   }
 });
 
-/*userSchema.statics.login = async function (email, password) {
-  const user = await this.findOne({ email });
-  if (user) {
-    const auth = await bcrypt.compare(password, user.password);
-    if (auth) {
-      return user;
-    }
-    throw Error("Incorrect password");
-  }
-  throw Error("Incorrect email");
-};*/
 userSchema.methods.login = async function (password) {
   const auth = await bcrypt.compare(password, this.password);
   if (auth) {
