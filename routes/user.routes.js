4e6e33a334cd3572bc14bb2b81551da34f8cbf68
Osiris8 <miganosiris8@gmaaail.com>
2023-08-18 17:55:05 +0200

    add login router

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,8 @@ const userController = require("../controllers/user.controller");
 
 ///Auth authentification
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+//router.get("/logout", authController.logout);
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
 router.put("/:id", userController.updateUser);
