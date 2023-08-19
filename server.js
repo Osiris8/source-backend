42128a727815ff2f5ecc05e1b7365714155ff944
Osiris8 <miganosiris8@gmaaail.com>
2023-08-19 11:35:14 +0200

    authentification all get route

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const userRoutes = require("./routes/user.routes");
 const app = express();
+const { authenticateMiddleware } = require("./middleware/auth.middleware");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("*", authenticateMiddleware);
 //routes
 app.use("/api/user", userRoutes);
 
