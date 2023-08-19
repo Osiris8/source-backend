4782334c657700274e8c74798976c6144c0ab764
Osiris8 <miganosiris8@gmaaail.com>
2023-08-19 19:49:20 +0200

    delete authentification middleware

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("*", authenticateMiddleware);
+//app.get("*", authenticateMiddleware);
 //routes
 app.use("/api/user", userRoutes);
 
