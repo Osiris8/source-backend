1420882c2628024f3f755b30419cadf996d097a5
Osiris8 <miganosiris8@gmaaail.com>
2023-08-21 14:36:32 +0200

    add like post route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,5 +5,6 @@ router.get("/", postController.readPost);
 router.post("/", postController.createPost);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
+router.patch("/like/:id", postController.likePost);
 
 module.exports = router;
