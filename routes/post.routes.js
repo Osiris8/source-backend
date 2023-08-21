df08d1fbc41899b0ecc250212b60432eb5bdc352
Osiris8 <miganosiris8@gmaaail.com>
2023-08-21 15:41:22 +0200

    add commentPost route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -7,5 +7,6 @@ router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
 router.patch("/like/:id", postController.likePost);
 router.patch("/unlike/:id", postController.unlikePost);
+router.patch("/comment/:id", postController.commentPost);
 
 module.exports = router;
