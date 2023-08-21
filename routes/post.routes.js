3a49a605489e69b54bf8df74f97209e257c6eedd
Osiris8 <miganosiris8@gmaaail.com>
2023-08-21 14:45:48 +0200

    add unlike route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -6,5 +6,6 @@ router.post("/", postController.createPost);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
 router.patch("/like/:id", postController.likePost);
+router.patch("/unlike/:id", postController.unlikePost);
 
 module.exports = router;
