1093e7a7855edf53eafdda73ebe1fc04ea3e469d
Osiris8 <miganosiris8@gmaaail.com>
2023-08-24 10:36:16 +0200

    add authentification Middleware for all routes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,14 +1,31 @@
 const router = require("express").Router();
 const postController = require("../controllers/post.controller");
+const auth = require("../middleware/auth.middleware");
 
-router.get("/", postController.readPost);
-router.post("/", postController.createPost);
-router.put("/:id", postController.updatePost);
-router.delete("/:id", postController.deletePost);
-router.patch("/like/:id", postController.likePost);
-router.patch("/unlike/:id", postController.unlikePost);
-router.patch("/comment/:id", postController.commentPost);
-router.patch("/editcomment/:id", postController.editComment);
-router.patch("/deletecomment/:id", postController.deleteComment);
+router.get("/", auth.authenticateMiddleware, postController.readPost);
+router.post("/", auth.authenticateMiddleware, postController.createPost);
+router.put("/:id", auth.authenticateMiddleware, postController.updatePost);
+router.delete("/:id", auth.authenticateMiddleware, postController.deletePost);
+router.patch("/like/:id", auth.authenticateMiddleware, postController.likePost);
+router.patch(
+  "/unlike/:id",
+  auth.authenticateMiddleware,
+  postController.unlikePost
+);
+router.patch(
+  "/comment/:id",
+  auth.authenticateMiddleware,
+  postController.commentPost
+);
+router.patch(
+  "/editcomment/:id",
+  auth.authenticateMiddleware,
+  postController.editComment
+);
+router.patch(
+  "/deletecomment/:id",
+  auth.authenticateMiddleware,
+  postController.deleteComment
+);
 
 module.exports = router;
