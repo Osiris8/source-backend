28f6d81f976f51aac56c0c0e6199c601a41ee409
Osiris8 <miganosiris8@gmaaail.com>
2023-08-24 11:55:39 +0200

    update upload profile controller

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,76 +1,57 @@
-const multer = require("multer");
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
-const userModel = require("../models/user.model"); // Assurez-vous de spécifier le bon chemin
+const multer = require("multer");
+const UserModel = require("../models/user.model");
 
-// Configuration de Multer pour le stockage des fichiers
+// Configuration de Multer pour gérer les fichiers d'images
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "../uploads/profiles"); // Répertoire où les fichiers seront stockés
+  destination: function (req, file, cb) {
+    cb(null, "../uploads/profiles"); // Dossier de destination des images
   },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}${ext}`);
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extname = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + extname);
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).single("profileImage"); // Nom du champ de fichier dans le formulaire
 
-// Contrôleur pour la modification de la photo de profil
-const updateProfilePhoto = async (req, res) => {
-  try {
-    const userId = req.params.id; // Supposons que vous passez l'ID utilisateur dans les paramètres d'URL
-    const user = await userModel.findById(userId).select("-password");
-
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: "L'utilisateur n'a pas été trouvé." });
+module.exports.updateProfilePicture = async (req, res) => {
+  upload(req, res, async (err) => {
+    if (err instanceof multer.MulterError) {
+      // Gérer les erreurs de Multer
+      return res.status(500).json({
+        message: "Une erreur est survenue lors du téléchargement de l'image.",
+      });
+    } else if (err) {
+      // Gérer d'autres erreurs
+      return res.status(500).json({
+        message: "Une erreur est survenue lors du traitement de l'image.",
+      });
     }
+
+    const userId = req.user._id; // Supposons que vous ayez les informations de l'utilisateur via le middleware d'authentification
 
     // Vérifier si l'utilisateur a déjà une photo de profil
-    if (user.picture) {
-      // Supprimer l'ancienne photo de profil du disque local
-      try {
-        await fs.unlink(user.picture);
-      } catch (error) {
-        console.error(
-          "Erreur lors de la suppression de l'ancienne photo :",
-          error
-        );
+    const existingUser = await UserModel.findById(userId);
+    if (
+      existingUser.picture !==
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    ) {
+      // Supprimer l'ancienne photo de profil du dossier local
+      const oldImagePath = path.join(__dirname, `../${existingUser.picture}`);
+      if (fs.existsSync(oldImagePath)) {
+        fs.unlinkSync(oldImagePath);
       }
     }
 
-    // Traitement de la mise à jour de la photo de profil
-    upload.single("profilePhoto")(req, res, async (err) => {
-      if (err instanceof multer.MulterError) {
-        return res.status(400).json({
-          message:
-            "Une erreur s'est produite lors du téléchargement du fichier.",
-        });
-      } else if (err) {
-        return res
-          .status(500)
-          .json({ message: "Une erreur interne s'est produite." });
-      }
+    // Mettre à jour la nouvelle photo de profil dans la base de données
+    existingUser.picture = req.file.path.replace(/\\/g, "/"); // Chemin de l'image pour la base de données
+    await existingUser.save();
 
-      // Mise à jour de l'URL de la photo de profil dans le modèle de l'utilisateur
-      if (req.file) {
-        user.picture = req.file.path; // Mise à jour de l'URL de la photo dans le modèle
-        await user.save();
-      }
-
-      return res
-        .status(200)
-        .json({ message: "Photo de profil mise à jour avec succès." });
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Une erreur interne s'est produite." });
-  }
-};
-
-module.exports = {
-  updateProfilePhoto,
+    res
+      .status(200)
+      .json({ message: "Photo de profil mise à jour avec succès." });
+  });
 };
