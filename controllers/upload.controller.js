91be7850d794fcb7a98be623c0747a4210583376
Osiris8 <miganosiris8@gmaaail.com>
2023-09-01 16:03:16 +0200

    update controller

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -6,7 +6,7 @@ const UserModel = require("../models/user.model");
 // Configuration de Multer pour gérer les fichiers d'images
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../uploads/profiles"); // Dossier de destination des images
+    cb(null, "../frontend/public/uploads/profiles"); // Dossier de destination des images
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage }).single("profileImage"); // Nom du champ de fichier dans le formulaire
+const upload = multer({ storage: storage }).single("image"); // Nom du champ de fichier dans le formulaire
 
 module.exports.updateProfilePicture = async (req, res) => {
   upload(req, res, async (err) => {
@@ -31,27 +31,37 @@ module.exports.updateProfilePicture = async (req, res) => {
       });
     }
 
-    const userId = req.user._id; // Supposons que vous ayez les informations de l'utilisateur via le middleware d'authentification
+    try {
+      const userId = req.params.id;
+      const user = await UserModel.findById(userId);
 
-    // Vérifier si l'utilisateur a déjà une photo de profil
-    const existingUser = await UserModel.findById(userId);
-    if (
-      existingUser.picture !==
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    ) {
-      // Supprimer l'ancienne photo de profil du dossier local
-      const oldImagePath = path.join(__dirname, `../${existingUser.picture}`);
-      if (fs.existsSync(oldImagePath)) {
-        fs.unlinkSync(oldImagePath);
+      if (!user) {
+        return res.status(404).json({ message: "Utilisateur non trouvé." });
       }
+
+      // Vérifiez si l'utilisateur a déjà une photo de profil personnalisée
+      if (
+        user.image !==
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+      ) {
+        // Supprimer l'ancienne photo de profil du dossier local
+        const oldImagePath = path.join(__dirname, `../${user.image}`);
+        if (fs.existsSync(oldImagePath)) {
+          fs.unlinkSync(oldImagePath);
+        }
+      }
+
+      // Mettez à jour le champ "image" dans le modèle user
+      user.image = req.file.path.replace(/\\/g, "/");
+      await user.save();
+
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour de l'utilisateur :", error);
+      res.status(500).json({
+        message:
+          "Une erreur s'est produite lors de la mise à jour de l'utilisateur.",
+      });
     }
-
-    // Mettre à jour la nouvelle photo de profil dans la base de données
-    existingUser.picture = req.file.path.replace(/\\/g, "/"); // Chemin de l'image pour la base de données
-    await existingUser.save();
-
-    res
-      .status(200)
-      .json({ message: "Photo de profil mise à jour avec succès." });
   });
 };
