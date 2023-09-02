1e4c8f930238e935a6b5f6db451ca5f4a6d78e47
Osiris8 <miganosiris8@gmaaail.com>
2023-09-02 23:56:26 +0200

    add timestamps field in userModel

--- models/user.model.js
+++ models/user.model.js
@@ -2,56 +2,61 @@ const mongoose = require("mongoose");
 const { isEmail } = require("validator");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 20,
-    unique: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+      unique: true,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minLength: 6,
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    biographie: {
+      type: String,
+      default: "Aucune biographie disponible",
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
   },
-  lastname: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 20,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    validate: [isEmail],
-    lowercase: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 1024,
-    minLength: 6,
-  },
-  image: {
-    type: String,
-    default:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-  },
-  biographie: {
-    type: String,
-    default: "Aucune biographie disponible",
-  },
-  followers: {
-    type: [String],
-  },
-  following: {
-    type: [String],
-  },
-  likes: {
-    type: [String],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Lancer cette fonction avant d'enregister
 userSchema.pre("save", async function (next) {
