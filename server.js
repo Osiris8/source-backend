04f30a8b29c226df7bd38ef6913ed8c5c84be19a
Osiris8 <miganosiris8@gmaaail.com>
2023-10-21 23:56:07 +0200

    update ser.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Autorisez les requêtes provenant de l'origine http://localhost:3000
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://source-frontend-psi.vercel.app/"],
+  origin: ["http://localhost:3000", "https://source-flame.vercel.app/"],
   credentials: true, // Permet d'envoyer les cookies et les headers d'authentification
 };
 
